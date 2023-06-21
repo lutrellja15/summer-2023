7afbfbc5443847a94e0d725398901216a5671974
baylee <133932148+bayleecaldwell@users.noreply.github.com>
2023-06-21 10:38:37 -0500

    Update Caldwell_Asg4P4.cpp

--- Caldwell_Asg4P4.cpp
+++ Caldwell_Asg4P4.cpp
@@ -7,38 +7,34 @@ Problem 4
 
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <algorithm>
-
 using namespace std;
 
-// function to get grocery items for a store
-void getGroceryItems(vector<string>& items) {
-    cout << "Enter five grocery items:" << endl;
-    for (int i = 0; i <= 5; i++) {
-        string item;
-        getline(cin, item);
-        items.push_back(item);
+const int MAX_ITEMS = 5;
+
+// Function to get grocery items for a store
+void getGroceryItems(string storeName, string items[]) {
+    cout << "Enter five grocery items for " << storeName << ":" << endl;
+    for (int i = 0; i < MAX_ITEMS; i++) {
+        cout << "Item " << (i + 1) << ": ";
+        getline(cin, items[i]);
     }
 }
 
-// function to display grocery items for a store
-void displayGroceryItems(vector<string>& items) {
-    cout << "Grocery items:" << endl;
-    
-    string items1;
-    for (int i=0; i<=4; i++) {
-        cout << "- " << items1 << endl;
+// Function to display grocery items for a store
+void displayGroceryItems(string storeName, string items[]) {
+    cout << "Grocery items for " << storeName << ":" << endl;
+    for (int i = 0; i < MAX_ITEMS; i++) {
+        cout << "- " << items[i] << endl;
     }
-
-  //  for (string item : items) {
-  //      cout << "- " << item << endl;
-  //  }
+    cout << endl;
 }
 
 int main() {
-    vector<string> walmartItems, hometownItems, samsItems;
+    string walmartItems[MAX_ITEMS], hometownItems[MAX_ITEMS], samsItems[MAX_ITEMS];
     char choice;
+    bool walmartEntered = false, hometownEntered = false, samsEntered = false;
+
     do {
         cout << "Select a grocery store:" << endl;
         cout << "W - Wal-Mart" << endl;
@@ -46,65 +42,75 @@ int main() {
         cout << "S - Sam's Club" << endl;
         cout << "D - Done" << endl;
         cin >> choice;
-        switch (choice) {
+        cin.ignore(); // Ignore the newline character left in the input buffer
+
+        switch (toupper(choice)) {
             case 'W':
-            case 'w':
-                getGroceryItems(walmartItems);
+                getGroceryItems("Wal-Mart", walmartItems);
+                walmartEntered = true;
                 break;
             case 'H':
-            case 'h':
-                getGroceryItems(hometownItems);
+                getGroceryItems("Hometown Market", hometownItems);
+                hometownEntered = true;
                 break;
             case 'S':
-            case 's':
-                getGroceryItems(samsItems);
+                getGroceryItems("Sam's Club", samsItems);
+                samsEntered = true;
                 break;
             case 'D':
-            case 'd':
                 break;
             default:
                 cout << "Invalid choice. Please try again." << endl;
                 break;
         }
-    } while (choice != 'D' && choice != 'd');
-    
+    } while (toupper(choice) != 'D');
+
     // Sort grocery items in alphabetical order
-    sort(walmartItems.begin(), walmartItems.end());
-    sort(hometownItems.begin(), hometownItems.end());
-    sort(samsItems.begin(), samsItems.end());
-    
+    sort(walmartItems, walmartItems + MAX_ITEMS);
+    sort(hometownItems, hometownItems + MAX_ITEMS);
+    sort(samsItems, samsItems + MAX_ITEMS);
+
     // Write grocery items to the output file
     ofstream outputFile("groceryList.txt");
     if (outputFile.is_open()) {
-        outputFile << "Grocery items for each store:" << endl;
-        outputFile << "Wal-Mart:" << endl;
-        for (string item : walmartItems) {
-            outputFile << "- " << item << endl;
+        outputFile << "Grocery List:" << endl;
+        if (walmartEntered) {
+            outputFile << "Wal-Mart:" << endl;
+            for (int i = 0; i < MAX_ITEMS; i++) {
+                outputFile << "- " << walmartItems[i] << endl;
+            }
+            outputFile << endl;
         }
-        outputFile << "Hometown Market:" << endl;
-        for (string item : hometownItems) {
-            outputFile << "- " << item << endl;
+        if (hometownEntered) {
+            outputFile << "Hometown Market:" << endl;
+            for (int i = 0; i < MAX_ITEMS; i++) {
+                outputFile << "- " << hometownItems[i] << endl;
+            }
+            outputFile << endl;
         }
-        outputFile << "Sam's Club:" << endl;
-        for (string item : samsItems) {
-            outputFile << "- " << item << endl;
+        if (samsEntered) {
+            outputFile << "Sam's Club:" << endl;
+            for (int i = 0; i < MAX_ITEMS; i++) {
+                outputFile << "- " << samsItems[i] << endl;
+            }
+            outputFile << endl;
         }
         outputFile.close();
         cout << "Grocery items have been written to groceryList.txt" << endl;
     } else {
         cout << "Unable to open the file." << endl;
-    }
-    
-    ifstream inputFile("groceryList.txt");
-    if (inputFile.is_open()) {
-        string line;
-        while (getline(inputFile, line)) {
-            cout << line << endl;
-        }
-        inputFile.close();
-    } else {
-        cout << "Unable to open the file." << endl;
-    }
+   }
+// Display grocery items
 
-    return 0;
+if (walmartEntered) {
+    displayGroceryItems("Wal-Mart", walmartItems);
+}
+if (hometownEntered) {
+    displayGroceryItems("Hometown Market", hometownItems);
+}
+if (samsEntered) {
+    displayGroceryItems("Sam's Club", samsItems);
+}
+
+return 0;
 }
