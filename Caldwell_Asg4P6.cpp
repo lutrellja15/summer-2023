4513c57965ae45baf77fab9ffd1cbef4225e96bb
baylee <133932148+bayleecaldwell@users.noreply.github.com>
2023-06-21 12:24:20 -0500

    Update Caldwell_Asg4P6.cpp

--- Caldwell_Asg4P6.cpp
+++ Caldwell_Asg4P6.cpp
@@ -4,7 +4,7 @@ Baylee Caldwell
 June 21, 2023
 Problem 6
 *****************/
-
+/*
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -50,6 +50,76 @@ int main() {
     }
 
     generateBarChart(closingPrices, outputFile);
+
+    inputFile.close();
+    outputFile.close();
+
+    cout << "Bar chart generated and written to StockResults.txt." << endl;
+
+    return 0;
+}
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void generateBarChart(const int* data, int size, ofstream& outputFile) {
+    int year = 2015;
+    for (int i = size - 1; i >= 0; i--) {
+        int average = data[i] / 3000;
+        outputFile << year << ": ";
+        for (int j = 0; j < average; j++) {
+            outputFile << '*';
+        }
+        outputFile << endl;
+        year++;
+        if (year > 2021) {
+            break;
+        }
+    }
+}
+
+int main() {
+    ifstream inputFile("test.txt");
+    ofstream outputFile("StockResults.txt");
+
+    if (!inputFile) {
+        cerr << "Error opening input file." << endl;
+        return 1;
+    }
+
+    if (!outputFile) {
+        cerr << "Error creating output file." << endl;
+        return 1;
+    }
+
+    int* closingPrices = nullptr;
+    int capacity = 10;
+    int size = 0;
+
+    closingPrices = new int[capacity];
+
+    int price;
+    while (inputFile >> price) {
+        if (size == capacity) {
+            // Double the capacity if the array is full
+            capacity *= 2;
+            int* temp = new int[capacity];
+            for (int i = 0; i < size; i++) {
+                temp[i] = closingPrices[i];
+            }
+            delete[] closingPrices;
+            closingPrices = temp;
+        }
+        closingPrices[size] = price;
+        size++;
+    }
+
+    generateBarChart(closingPrices, size, outputFile);
+
+    delete[] closingPrices;
 
     inputFile.close();
     outputFile.close();
